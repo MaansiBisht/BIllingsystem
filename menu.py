bdb6444a7f85e17ae8eb8c619c33cdac7e81fb7e
MaansiBisht <66878185+MaansiBisht@users.noreply.github.com>
2021-08-01 23:42:33 +0530

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,37 @@
+(i__main__restaurantp0 (dp1S'sno'p2I111sS'price'p3F100.0sS'name'p4
+S'Cold Coffee'p5
+sb.(i__main__restaurantp0(dp1S'sno'p2I112sS'price'p3F120.0sS'name'
+p4S'Cappuccino'p5
+sb.(i__main__restaurantp0 (dp1S'sno'p2I113sS'price'p3F150.0sS'name'
+p4S'Aztec'p5
+sb.(i__main__restaurantp0(dp1S'sno'p2I114sS'price'p3F150.0sS'name'
+p4S'Frappe'p5
+sb.(i__main__restaurantp0(dp1S'sno'p2I115sS'price'p3F120.0sS'name'
+p4S'Shakes'p5
+sb.(i__main__restaurantp0(dp1S'sno'p2I121sS'price'p3F120.0sS'name'
+p4S'Garlic Bread'p5
+sb. (i__main__Restaurantp0(dp1S'sno'p2I122sS'price'p3F150.0sS'name'
+p4S'Nachos'p5
+sb.(i__main__restaurantp0(dp1S'sno'p2I123sS'price'p3F160.0sS'name'
+p4S'Margherita'p5
+sb.(i__main__restaurantp0(dp1S'sno'p2I124sS'price'p3F200.0sS'name'
+p4S'Chessy Pesto'p5
+sb.(i__main__restaurantp0(dp1S'sno'p2I125sS'price'p3F100.0sS'name'
+p4S'Burger'p5
+sb.(i__main__restaurantp0(dp1S'sno'p2I131sS'price'p3F130.0sS'name'
+p4S'Red Velvet'p5
+sb.(i__main__restaurantp0(dp1S'sno'p2I132sS'price'p3F160.0sS'name'
+p4S'Tiramisu'p5
+sb.(i__main__restaurantp0(dp1S'sno'p2I133sS'price'p3F180.0sS'name'
+p4S'Waffles'p5
+sb.(i__main__Restaurantp0(dp1S'sno'p2I134sS'price'p3F120.0sS'name'
+p4S'Chessecake'p5
+sb.(i__main__restaurantp0(dp1S'sno'p2I135sS'price'p3F100.0sS'name'
+p4S'Brownie'p5
+sb.(i__main__restaurantp0(dp1S'sno'p2I136sS'price'p3F350.0sS'name'
+p4S'choco truffle'p5
+sb.(i__main__restaurantp0(dp1S'sno'p2I138sS'price'p3F200.0sS'name'
+p4S'exotic calazone'p5
+sb.(i__main__restaurantp0(dp1S'sno'p2I139sS'price'p3F150.0sS'name'
+p4S'philly cheese'p5
+sb.
